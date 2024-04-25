24d5b845445b480ad6388f93b4f37a67488a912a
Yunseong Jeong <jyerd333@gmail.com>
2024-04-25 19:28:01 +0900

    users DB 생성

---
+++ Back/src/main/java/com/capstone/usa/Repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.capstone.usa.Repository;
+
+import com.capstone.usa.Model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, String> {
+}
