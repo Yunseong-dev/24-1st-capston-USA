f54edea3945c401136fcba2e6cfb31755d85c85d
Yunseong Jeong <jyerd333@gmail.com>
2024-04-26 19:05:56 +0900

    test 수정

--- Back/src/test/java/com/capstone/usa/UsaApplicationTests.java
+++ Back/src/test/java/com/capstone/usa/UsaApplicationTests.java
@@ -1,11 +1,7 @@
 package com.capstone.usa;
 
-import com.capstone.usa.service.VerificationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class UsaApplicationTests {
