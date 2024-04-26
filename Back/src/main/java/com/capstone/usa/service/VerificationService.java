9bda760d6fa1df370131e1414190a7aa014c6edf
Yunseong Jeong <jyerd333@gmail.com>
2024-04-26 17:36:06 +0900

    랜덤 4자리 숫자 생성

---
+++ Back/src/main/java/com/capstone/usa/service/VerificationService.java
@@ -0,0 +1,23 @@
+package com.capstone.usa.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class VerificationService {
+    private static Map<String, String> verificationCodes = new HashMap<>();
+
+    public static String GenerateNumber(String phoneNumber){
+        Random random = new Random();
+        int randomNumber = random.nextInt(10000);
+        String verificationCode = String.valueOf(randomNumber);
+
+        verificationCodes.put(phoneNumber, verificationCode);
+
+        return verificationCode;
+    }
+
+    public static String getVerificationCode(String phoneNumber) {
+        return verificationCodes.get(phoneNumber);
+    }
+}
