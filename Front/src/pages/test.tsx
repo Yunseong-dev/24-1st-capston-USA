75ce1f3a04aae1e4ad2b95719727fd881b74024a
Yunseong Jeong <jyerd333@gmail.com>
2024-04-26 11:33:33 +0900

    회원가입 오류 추가

--- Front/src/pages/test.tsx
+++ Front/src/pages/test.tsx
@@ -1,18 +1,32 @@
 import { useState } from 'react';
 import { customAxios } from "../utils/axios";
+import { useNavigate } from 'react-router-dom';
 
 const test = () => {
    const [name, setName] = useState("");
    const [phoneNumber, setPhone] = useState("");
 
-   const joinHandler = async () => {
+   const navigate = useNavigate();
 
+   const joinHandler = async (e: { preventDefault: () => void; }) => {
+      e.preventDefault()
 
-      const response = await customAxios.post('/api/user/signup', {
-         name,
-         phoneNumber
-      });
-
+      try {
+         const response = await customAxios.post('/api/user/signup', {
+            name,
+            phoneNumber
+         });
+         if (response.status === 200) {
+            alert("회원가입에 성공하였습니다")
+            navigate('/2')
+         } else {
+            alert('가입은 성공적으로 되었으나 상태 코드가 200이 아닙니다.');
+         }
+      } catch (error: any) {
+         if (error.response.status === 500) {
+            alert("이미 존재하는 전화번호 입니다.")
+         }
+      }
    }
 
    return (
