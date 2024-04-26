b9ed2978172e6bafe9661214223a08d1c0670129
Yunseong Jeong <jyerd333@gmail.com>
2024-04-26 17:08:04 +0900

    4자리 숫자 생성 테스트

--- Back/src/test/java/com/capstone/usa/UsaApplicationTests.java
+++ Back/src/test/java/com/capstone/usa/UsaApplicationTests.java
@@ -1,13 +1,30 @@
 package com.capstone.usa;
 
+import com.capstone.usa.service.VerificationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class UsaApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testGenerateNumber() {
+        String generatedNumber = VerificationService.GenerateNumber();
+
+		assertEquals(4, generatedNumber.length());
+
+		for (char digit : generatedNumber.toCharArray()) {
+			assertTrue(Character.isDigit(digit));
+		}
+
+		System.out.println("Generated number: " + generatedNumber);
 	}
 
 }
