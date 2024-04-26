6afb75932b4c702bc7f8746e4eba0f869928418b
Yunseong Jeong <jyerd333@gmail.com>
2024-04-26 18:25:48 +0900

    인증번호 체킹 추가

--- Front/src/pages/test.tsx
+++ Front/src/pages/test.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 const test = () => {
    const [name, setName] = useState("");
    const [phoneNumber, setPhone] = useState("");
+   const [verNumber, setVerNumber] = useState("")
 
    const navigate = useNavigate();
 
@@ -22,14 +23,15 @@ const test = () => {
             return
          }
 
-         if(!isValidPhoneNumber(phoneNumber)){
+         if (!isValidPhoneNumber(phoneNumber)) {
             alert('올바른 전화번호 형식이 아닙니다.')
             return
          }
 
          const response = await customAxios.post('/api/user/signup', {
             name,
-            phoneNumber
+            phoneNumber,
+            verNumber
          });
 
          alert("회원가입에 성공하였습니다")
@@ -45,9 +47,26 @@ const test = () => {
    const checkPhoneHandler = async (e: { preventDefault: () => void; }) => {
       e.preventDefault()
 
-      const response = await customAxios.post('/api/user/SMS', {
-         phoneNumber
-      });
+      try {
+         if (!name || !phoneNumber) {
+            alert('모든 입력란을 작성해주세요.')
+            return
+         }
+
+         if (!isValidPhoneNumber(phoneNumber)) {
+            alert('올바른 전화번호 형식이 아닙니다.')
+            return
+         }
+
+         const response = await customAxios.post('/api/sms', {
+            phoneNumber
+         });
+
+      } catch (error: any) {
+         if (error.response.status === 500) {
+            alert("이미 존재하는 전화번호 입니다.")
+         }
+      }
    }
 
    return (
@@ -55,6 +74,7 @@ const test = () => {
          <form>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="이름" />
             <input type="text" value={phoneNumber} onChange={(e) => setPhone(e.target.value)} placeholder="전화번호" />
+            <input type="text" value={verNumber} onChange={(e) => setVerNumber(e.target.value)} placeholder="인증번호" />
             <button onClick={checkPhoneHandler}>인증코드 전송</button>
             <button onClick={joinHandler}>회원가입</button>
          </form>
