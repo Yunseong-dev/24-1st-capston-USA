44c0e1ed44e5cc2276e66d8a11f2312679c3b895
Yunseong Jeong <jyerd333@gmail.com>
2024-04-26 18:43:13 +0900

    CSRF 허용

---
+++ Back/src/main/java/com/capstone/usa/utils/WebConfig.java
@@ -0,0 +1,17 @@
+package com.capstone.usa.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "http://localhost")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
