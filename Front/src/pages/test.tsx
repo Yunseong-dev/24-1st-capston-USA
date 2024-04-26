747bb15ecc71ced463a7a67930db259923070710
Yunseong Jeong <jyerd333@gmail.com>
2024-04-26 11:21:07 +0900

    post 변경

--- Front/src/pages/test.tsx
+++ Front/src/pages/test.tsx
@@ -3,14 +3,14 @@ import { customAxios } from "../utils/axios";
 
 const test = () => {
    const [name, setName] = useState("");
-   const [phone, setPhone] = useState("");
+   const [phoneNumber, setPhone] = useState("");
 
    const joinHandler = async () => {
 
 
       const response = await customAxios.post('/api/user/signup', {
          name,
-         phone
+         phoneNumber
       });
 
    }
@@ -19,7 +19,7 @@ const test = () => {
       <div>
          <form onSubmit={joinHandler}>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="이름" />
-            <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="전화번호" />
+            <input type="text" value={phoneNumber} onChange={(e) => setPhone(e.target.value)} placeholder="전화번호" />
             <button type="submit">회원가입</button>
          </form>
       </div>
