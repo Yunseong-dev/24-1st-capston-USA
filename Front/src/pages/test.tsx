9cb15ad17bc64bef4babe1fd261cdfa5efd803cd
Yunseong Jeong <jyerd333@gmail.com>
2024-04-26 11:11:09 +0900

    submit 버튼 추가

--- Front/src/pages/test.tsx
+++ Front/src/pages/test.tsx
@@ -20,6 +20,7 @@ const test = () => {
          <form onSubmit={joinHandler}>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="이름" />
             <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="전화번호" />
+            <button type="submit">회원가입</button>
          </form>
       </div>
    )
