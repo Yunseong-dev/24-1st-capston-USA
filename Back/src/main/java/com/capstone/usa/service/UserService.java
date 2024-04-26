31ccc9e44d670e32d5aa2ec1587e10e4d1bac241
Yunseong Jeong <jyerd333@gmail.com>
2024-04-26 11:21:33 +0900

    User 저장중 전화번호가 존재한다면 추가

--- Back/src/main/java/com/capstone/usa/service/UserService.java
+++ Back/src/main/java/com/capstone/usa/service/UserService.java
@@ -13,10 +13,15 @@ public class UserService {
     private UserRepository userRepository;
 
     public User createUser(CreateUserDto dto) {
-        User user = new User(
-                dto.getName(),
-                dto.getPhoneNumber()
-        );
-        return userRepository.save(user);
+        if (userRepository.findByPhoneNumber(dto.getPhoneNumber()) != null) {
+            throw new IllegalArgumentException("이미 존재하는 전화번호입니다.");
+        }
+        else {
+            User user = new User(
+                    dto.getName(),
+                    dto.getPhoneNumber()
+            );
+            return userRepository.save(user);
+        }
     }
 }
