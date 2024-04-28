c055021cf3707aafa3d80caff109adf726a13528
Yunseong Jeong <jyerd333@gmail.com>
2024-04-28 17:28:21 +0900

    findByPhoneNumber 삭제

--- Back/src/main/java/com/capstone/usa/user/service/UserService.java
+++ Back/src/main/java/com/capstone/usa/user/service/UserService.java
@@ -2,9 +2,10 @@ package com.capstone.usa.user.service;
 
 import com.capstone.usa.sms.service.VerificationService;
 import com.capstone.usa.user.dto.CreateUserDto;
+import com.capstone.usa.user.dto.LoginUserDto;
 import com.capstone.usa.user.model.User;
 import com.capstone.usa.user.repository.UserRepository;
-import com.capstone.usa.user.util.JwtUtil;
+import com.capstone.usa.security.JwtUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,23 +21,27 @@ public class UserService {
     private UserRepository userRepository;
     private final JwtUtil jwtUtil;
 
-    public ResponseEntity<?> loginUser(CreateUserDto dto) {
-        Map<String, Object> response = new HashMap<>();
+    public ResponseEntity<?> createUser(CreateUserDto dto) {
+        VerificationService.testCode();
         String savedVerificationCode = VerificationService.getVerificationCode(dto.getPhoneNumber());
 
-        if (savedVerificationCode != null && savedVerificationCode.equals(dto.getVerNumber())) {
-            User user = new User(dto.getName(), dto.getPhoneNumber());
-            userRepository.save(user);
-            VerificationService.deleteVerificationCode(dto.getPhoneNumber());
+        if(userRepository.findById(dto.getPhoneNumber()) == null){
 
-            String token = jwtUtil.generateToken(user);
-
-            response.put("token", token);
-            response.put("message", "사용자가 성공적으로 생성되었습니다.");
-
-            return ResponseEntity.ok().body(response);
-        } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("인증번호가 일치하지 않습니다.");
         }
+
+        if (savedVerificationCode != null && savedVerificationCode.equals(dto.getVerNumber())) {
+                User user = new User(dto.getName(), dto.getPhoneNumber(), dto.getPassword());
+                userRepository.save(user);
+                VerificationService.deleteVerificationCode(dto.getPhoneNumber());
+
+                return ResponseEntity.ok().body("사용자가 성공적으로 생성되었습니다");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("인증번호가 일치하지 않습니다");
+        }
+    }
+
+    public ResponseEntity<?> loginUser(LoginUserDto dto) {
+
+        return ResponseEntity.ok().body("로그인이 완료돼었습니다");
     }
 }
