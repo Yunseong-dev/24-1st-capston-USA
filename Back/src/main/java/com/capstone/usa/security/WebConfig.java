2c170c94706c7f3bf5cf8e93cf0ba400cebee0c1
Yunseong Jeong <jyerd333@gmail.com>
2024-04-28 15:14:57 +0900

    위치 이동

--- Back/src/main/java/com/capstone/usa/security/WebConfig.java
+++ Back/src/main/java/com/capstone/usa/security/WebConfig.java
@@ -1,4 +1,4 @@
-package com.capstone.usa.utils;
+package com.capstone.usa.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
