14f206eff553e2d4097b5e94d90e61259cf02e2c
Yunseong Jeong <jyerd333@gmail.com>
2024-04-28 17:26:20 +0900

    회원가입 수정, 로그인 생성

---
+++ Front/src/pages/signin.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react"
+import { customAxios } from "../utils/axios"
+import { useNavigate } from 'react-router-dom'
+import useToken from "../hooks/useToken"
+
+const signin = () => {
+   const [phoneNumber, setPhone] = useState("")
+   const [password, setPassword] = useState("")
+
+   const navigate = useNavigate()
+
+   const { token, setToken } = useToken()
+
+   useEffect(() => {
+      if (!!token) {
+         alert("이미 로그인이 되어있습니다.")
+         navigate("/")
+      }
+   }, [token, navigate])
+
+   const joinHandler = async () => {
+      try {
+         if (!phoneNumber || !password) {
+            alert('모든 입력란을 작성해주세요')
+            return
+         }
+
+         const response = await customAxios.post('/api/user/signin', {
+            phoneNumber,
+            password
+         })
+
+         navigate("/")
+         alert("로그인이 완료되었습니다")
+
+      } catch (error: any) {
+         if (error.response && error.response.data) {
+            alert(error.response.data)
+         }
+      }
+   }
+
+   return (
+      <div>
+         <form>
+            <input type="text" value={phoneNumber} onChange={(e) => setPhone(e.target.value)} placeholder="전화번호" />
+            <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="비밀번호" />
+            <button onClick={joinHandler}>로그인</button>
+         </form>
+      </div>
+   )
+}
+
+export default signin
---
+++ Front/src/pages/signup.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react"
+import { customAxios } from "../utils/axios"
+import { useNavigate } from 'react-router-dom'
+import useToken from "../hooks/useToken"
+
+const test = () => {
+   const [name, setName] = useState("")
+   const [phoneNumber, setPhone] = useState("")
+   const [verNumber, setVerNumber] = useState("")
+   const [password, setPassword] = useState("")
+   const [ckpassword, setCkPassword] = useState("")
+
+   const navigate = useNavigate()
+
+   const isValidPhoneNumber = (phoneNumber: string) => {
+      const regex = /^\d{11}$/
+      return regex.test(phoneNumber)
+   }
+
+   const joinHandler = async (e: { preventDefault: () => void }) => {
+      e.preventDefault()
+
+      try {
+         if (!name || !phoneNumber || !verNumber || !password || !ckpassword) {
+            alert('모든 입력란을 작성해주세요')
+            return
+         }
+
+         if (password != ckpassword) {
+            alert('비밀번호와 비밀번호확인이 일치하지 않습니다')
+            return
+         }
+
+         const response = await customAxios.post('/api/user/signup', {
+            name,
+            phoneNumber,
+            verNumber
+         })
+
+         navigate('/')
+         alert("회원가입에 성공하였습니다")
+
+      } catch (error: any) {
+         if (error.response && error.response.data) {
+            alert(error.response.data)
+         }
+      }
+   }
+
+   const checkPhoneHandler = async (e: { preventDefault: () => void }) => {
+      e.preventDefault()
+
+      try {
+         if (!phoneNumber) {
+            alert("전화번호를 작성해주세요")
+            return
+         }
+
+         if (!isValidPhoneNumber(phoneNumber)) {
+            alert("올바른 전화번호 형식이 아닙니다")
+            return
+         }
+
+         const response = await customAxios.post('/api/sms', {
+            phoneNumber
+         })
+
+         alert("인증번호가 전송되었습니다")
+
+      } catch (error: any) {
+         if (error.response && error.response.data) {
+            alert("오류가 발생했습니다")
+         }
+      }
+   }
+
+   return (
+      <div>
+         <form>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="이름" />
+            <input type="text" value={phoneNumber} onChange={(e) => setPhone(e.target.value)} placeholder="전화번호" />
+            <input type="text" value={verNumber} onChange={(e) => setVerNumber(e.target.value)} placeholder="인증번호" />
+            <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="비밀번호" />
+            <input type="text" value={ckpassword} onChange={(e) => setCkPassword(e.target.value)} placeholder="비밀번호확인" />
+            <button onClick={checkPhoneHandler}>인증코드 전송</button>
+            <button onClick={joinHandler}>회원가입</button>
+         </form>
+      </div>
+   )
+}
+
+export default test
