4b1dcfcbfcefdea5a3e6ec762fd13bd93ebd3634
Yunseong Jeong <jyerd333@gmail.com>
2024-04-28 18:44:02 +0900

    줄바꿈

--- Front/src/pages/main.tsx
+++ Front/src/pages/main.tsx
@@ -4,7 +4,7 @@ const main = () => {
 
    return (
       <div>
-         <a href="/signup">회원가입</a>
+         <a href="/signup">회원가입</a> <br/>
          <a href="/signin">로그인</a>
       </div>
    )
