85e9699472466942bd4c4fd3f6f024ac5fe7bb50
Yunseong Jeong <jyerd333@gmail.com>
2024-04-29 11:53:05 +0900

    Renamed

--- Back/src/main/java/com/capstone/usa/security/SecurityConfig.java
+++ Back/src/main/java/com/capstone/usa/security/SecurityConfig.java
@@ -18,7 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http,
