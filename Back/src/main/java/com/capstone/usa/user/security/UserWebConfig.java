f29770cc2b772caaa2adfd4d10b669ec1a5a7d87
Yunseong Jeong <jyerd333@gmail.com>
2024-06-13 18:05:52 +0900

    이름 수정

--- Back/src/main/java/com/capstone/usa/user/security/UserWebConfig.java
+++ Back/src/main/java/com/capstone/usa/user/security/UserWebConfig.java
@@ -5,13 +5,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class UserWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173", "http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .exposedHeaders("Authorization"); // 허용할 추가적인 헤더
+
     }
 }
