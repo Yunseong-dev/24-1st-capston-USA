64cc6b8964a8f77e499c0d9f6a94f22238f6b069
Yunseong Jeong <jyerd333@gmail.com>
2024-06-19 15:47:45 +0900

    Error 표시 변경

--- Front/src/pages/job/JobPost.tsx
+++ Front/src/pages/job/JobPost.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { JobPost } from '../../interface/jobPost';
 import dayjs from 'dayjs';
-import { customAxios } from '../../utils/axios';
+import { customAxios, postWithToken } from '../../utils/axios';
 import useToken from '../../hooks/useToken';
 
 const JobPosts: React.FC = () => {
@@ -15,10 +15,8 @@ const JobPosts: React.FC = () => {
          try {
             const response = await customAxios.get<JobPost[]>('/job');
             setJobPosts(response.data);
-         } catch (error: any) {
-            if (error.response && error.response.data) {
-               alert(error.response.data)
-            }
+         } catch (error) {
+            alert("게시물을 가져올 수 없습니다")
          }
       };
       fetchJobPosts();
@@ -26,11 +24,7 @@ const JobPosts: React.FC = () => {
 
    const handleChat = async (jobId: number) => {
       try {
-         const response = await customAxios.post(`/chat/create/${jobId}`, {}, {
-            headers: {
-               'Authorization': `Bearer ${token}`
-            }
-         });
+         const response = await postWithToken(token, `/chat/create/${jobId}`, "");
 
          const roomId = response.data.roomId;
          navigate(`/chat/${roomId}`);
