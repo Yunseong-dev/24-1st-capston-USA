a70253efa8b2b0eee1090cbdce5754dfb47b2759
Yunseong Jeong <jyerd333@gmail.com>
2024-06-20 20:04:01 +0900

    MessageDto 추가

--- Back/src/main/java/com/capstone/usa/chat/dto/MessageDto.java
+++ Back/src/main/java/com/capstone/usa/chat/dto/MessageDto.java
@@ -4,5 +4,8 @@ import lombok.Getter;
 
 @Getter
 public class MessageDto {
+    private String chatRoomId;
+    private String messageType;
     private String message;
+    private String sender;
 }
