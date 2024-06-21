f30f82dd681e865e44de7793ebdbbe6b8beb3ed3
Yunseong Jeong <jyerd333@gmail.com>
2024-06-21 15:14:35 +0900

    채팅방 링크 추가

--- Front/src/pages/Home.tsx
+++ Front/src/pages/Home.tsx
@@ -6,7 +6,8 @@ const main = () => {
          <a href="/signup">회원가입</a> <br/>
          <a href="/signin">로그인</a> <br/>
          <a href="/ArticleList">게시물</a> <br/>
-         <a href="/JobList">구인구직</a>
+         <a href="/JobList">구인구직</a> <br />
+         <a href="/ChatList">채팅</a> <br />
       </div>
    )
 }
