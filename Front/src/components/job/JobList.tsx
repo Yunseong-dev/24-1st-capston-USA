1b4feb0445d7254f17853ba600a336cf8349c9e2
Yunseong Jeong <jyerd333@gmail.com>
2024-06-26 22:04:55 +0900

    style 추가

--- Front/src/components/job/JobList.tsx
+++ Front/src/components/job/JobList.tsx
@@ -3,15 +3,18 @@ import { customAxios } from '../../utils/axios';
 import dayjs from 'dayjs';
 import { Job } from '../../interface/job';
 import { Link } from 'react-router-dom';
+import styles from "../../css/job.module.css";
+import Header from '../header';
 
 const JobPosts: React.FC = () => {
-   const [jobPosts, setJobPosts] = useState<Job[]>([]);
+   const [jobs, setJobs] = useState<Job[]>([]);
+   const [searchTerm, setSearchTerm] = useState("");
 
    useEffect(() => {
       const fetchJobPosts = async () => {
          try {
             const response = await customAxios.get<Job[]>('/jobs');
-            setJobPosts(response.data);
+            setJobs(response.data);
          } catch (error) {
             alert("게시물을 가져올 수 없습니다")
          }
@@ -19,20 +22,44 @@ const JobPosts: React.FC = () => {
       fetchJobPosts();
    }, []);
 
+   const filteredJobs = jobs.filter((jobs) =>
+      jobs.title.toLowerCase().includes(searchTerm.toLowerCase())
+   );
+
    return (
       <div>
-         <a href="/createJob">게시물 생성</a>
-         <div>
-            {jobPosts.map(job => (
-               <div>
+         <Header />
+         <div className={styles.main}>
+            <div className={styles.search_container}>
+               <input
+                  type="text"
+                  className={styles.search_input}
+                  placeholder="검색어를 입력해 주세요."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+               />
+            </div>
+
+            <div className={styles.write}>
+               <Link to="/CreateJob">
+                  <button className={styles.write_button}>글 작성하기</button>
+               </Link>
+            </div>
+            
+            <div className={styles.box_container}>
+               {filteredJobs.map(job => (
                   <Link to={`/job/${job.id}`}>
-                     <h2>{job.title}</h2>
-                     <p>{dayjs(job.createdAt).format("YYYY년 MM월 DD일")}</p>
+                     <div className={styles.box_item}>
+                        <p className={styles.title}>{job.title}</p>
+                        <p>{job.content}</p>
+                        <p>{dayjs(job.createdAt).format("YYYY년 MM월 DD일")}</p>
+                     </div>
                   </Link>
-               </div>
-            ))}
+               ))}
+            </div>
          </div>
       </div>
+
    );
 };
 
