486a375b6abf5ff3c11be1a5c67ca6a8f67fb36c
Yunseong Jeong <jyerd333@gmail.com>
2024-06-27 08:37:28 +0900

    디자인 추가

--- Front/src/components/job/CreateJob.tsx
+++ Front/src/components/job/CreateJob.tsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useToken from "../../hooks/useToken";
 import { postWithToken } from "../../utils/axios";
+import Header from '../header';
+import styles from '../../css/createJob.module.css';
 
 const CreateArticle = () => {
    const [title, setTitle] = useState('');
    const [content, setContent] = useState('');
 
    const { token } = useToken()
-   
+
    const navigate = useNavigate();
 
    useEffect(() => {
@@ -24,6 +26,10 @@ const CreateArticle = () => {
       const data = { title, content };
 
       try {
+         if(!title || ! content){
+            return alert("모든 항목을 작성해주세요")
+         }
+
          await postWithToken(token, '/jobs', data);
 
          alert("게시물이 성공적으로 등록되었습니다");
@@ -35,25 +41,36 @@ const CreateArticle = () => {
 
    return (
       <div>
-         <h1>게시물 작성</h1>
-         <form onSubmit={createArticle}>
-            <input
-               type="text"
-               value={title}
-               onChange={(e) => setTitle(e.target.value)}
-               placeholder="제목"
-            />
-            <br />
-            <textarea
-               value={content}
-               onChange={(e) => setContent(e.target.value)}
-               placeholder="내용"
-               cols={30}
-               rows={10}
-            />
-            <br />
-            <button type="submit">작성</button>
-         </form>
+         <Header />
+         <div className={styles.main}>
+            <div className={styles.box_container}>
+               <h1>구인공고 등록</h1>
+               <div className={styles.box}>
+                  <form onSubmit={createArticle}>
+                     <p>제목</p>
+                     <div className={styles.title}>
+                        <input
+                           type="text"
+                           value={title}
+                           onChange={(e) => setTitle(e.target.value)}
+                           placeholder="제목"
+                        />
+                     </div>
+                     <p>상세 정보</p>
+                     <div className={styles.detail}>
+                        <textarea
+                           value={content}
+                           onChange={(e) => setContent(e.target.value)}
+                           placeholder="내용"
+                        />
+                     </div>
+                     <div className={styles.create_button}>
+                        <button type="submit">등록하기</button>
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </div>
       </div>
    );
 };
