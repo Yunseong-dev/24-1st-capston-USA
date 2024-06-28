09b9ec8910cb1b88cc9408c3371768054262ac99
Yunseong Jeong <jyerd333@gmail.com>
2024-06-28 12:17:39 +0900

    CORS URL 변경

--- Back/src/main/java/com/capstone/usa/auth/security/SecurityConfig.java
+++ Back/src/main/java/com/capstone/usa/auth/security/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://24-1st-capston-usa.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("https://24-1st-capston-usa.vercel.app", "http://usa-front.s3-website.ap-northeast-2.amazonaws.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
