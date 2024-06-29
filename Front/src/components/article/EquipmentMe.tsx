173e43d9f5d32273c3723705c5bcc64abdc073b2
Yunseong Jeong <jyerd333@gmail.com>
2024-06-29 19:25:15 +0900

    디자인 수정

--- Front/src/components/article/EquipmentMe.tsx
+++ Front/src/components/article/EquipmentMe.tsx
@@ -26,28 +26,30 @@ const EquipmentMe = () => {
 
     if (articles.length === 0) {
         return <div>
-                <Header />
+            <Header />
+            <div className={styles.main}>
                 <h3>빌린 장비가 없습니다</h3>
-                </div>
-                ;
+            </div>
+        </div>
+            ;
     }
 
     return (
         <div>
             <Header />
             <div className={styles.main}>
-                <div className={`${styles.grid_container} ${styles.center}`}>                    
-                {articles.map((rent) => (
-                    <Link to={`/equipment/${rent.id}`} key={rent.id}>
-                        <div className={styles.grid_item}>
-                            {rent.imgUrl && (
-                                <img src={rent.imgUrl} alt="Article Image" className={styles.image} />
-                            )}
-                            <p>모델명: {rent.title}</p>
-                            <p>대여일: {dayjs(rent.rentAt).format("YYYY년 MM월 DD일")}</p>
-                        </div>
-                    </Link>
-                ))}
+                <div className={`${styles.grid_container} ${styles.center}`}>
+                    {articles.map((rent) => (
+                        <Link to={`/equipment/${rent.id}`} key={rent.id}>
+                            <div className={styles.grid_item}>
+                                {rent.imgUrl && (
+                                    <img src={rent.imgUrl} alt="Article Image" className={styles.image} />
+                                )}
+                                <p>모델명: {rent.title}</p>
+                                <p>대여일: {dayjs(rent.rentAt).format("YYYY년 MM월 DD일")}</p>
+                            </div>
+                        </Link>
+                    ))}
                 </div>
             </div>
         </div>
